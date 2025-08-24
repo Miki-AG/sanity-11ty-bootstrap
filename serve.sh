503dda25124ba34014e68a9a41d4b5c46072c3d2
Miki-AG <miguel.a.garcia@ocecat.com>
2025-08-23 23:58:26 -0400

    Fix

--- serve.sh
+++ serve.sh
@@ -65,9 +65,24 @@ echo "Starting Sanity listener for real-time updates... (log: $LOG_DIR/listener.
 echo "Waiting for servers to start..."
 sleep 8
 
-# Open browser tabs
-echo "Opening browser tabs..."
-open http://localhost:8080
-open http://localhost:3333
+# Open browser tabs in Google Chrome
+echo "Opening browser tabs in Google Chrome..."
+osascript <<'END'
+tell application "Google Chrome"
+    if not (exists window 1) then make new window
+    set screen_bounds to bounds of window 1
+    set screen_width to item 3 of screen_bounds
+    set screen_height to item 4 of screen_bounds
+    set half_width to screen_width / 2
+
+    set window1 to make new window with properties {bounds:{0, 22, half_width, screen_height}}
+    set URL of active tab of window1 to "http://localhost:8080"
+
+    set window2 to make new window with properties {bounds:{half_width, 22, screen_width, screen_height}}
+    set URL of active tab of window2 to "http://localhost:3333"
+
+    activate
+end tell
+END
 
 echo "Servers are running in the background. Use ./stop.sh to stop them."
